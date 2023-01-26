aed7d184b2738fea859c323b5af51fc4cc46e2ef
dcmRobin <dcm.robin@gmail.com>
2023-01-26 17:30:47 +0000

    made the vending machine buttons be randomly off and on

--- Assets/Superlemonal/Scripts/vendingButton.cs
+++ Assets/Superlemonal/Scripts/vendingButton.cs
@@ -21,4 +21,46 @@ public class vendingButton : MonoBehaviour
     public Material yellowMaterial;
     public Material orangeMaterial;
     public bool hasBeenPressed = false;
+    public int randNum = 0;
+
+    void Start()
+    {
+        randNum = UnityEngine.Random.Range(0, 2);
+        if (randNum == 0)
+        {
+            hasBeenPressed = false;
+        }
+        else if (randNum == 1)
+        {
+            hasBeenPressed = true;
+        }
+        if (can == canColor.Red && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = redMaterial;
+        }
+        else if (can == canColor.Blue && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = blueMaterial;
+        }
+        else if (can == canColor.Green && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = greenMaterial;
+        }
+        else if (can == canColor.Purple && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = purpleMaterial;
+        }
+        else if (can == canColor.LightBlue && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = lightblueMaterial;
+        }
+        else if (can == canColor.Yellow && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = yellowMaterial;
+        }
+        else if (can == canColor.Orange && hasBeenPressed)
+        {
+            gameObject.GetComponent<Renderer>().material = orangeMaterial;
+        }
+    }
 }
