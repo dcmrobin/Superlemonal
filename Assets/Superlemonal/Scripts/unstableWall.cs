b64ec27541aa51ba8a9fe0025dc3abe0d9c21464
dcmRobin <dcm.robin@gmail.com>
2023-06-28 10:06:06 +0100

    fixed the problem where the player could push over the wall

---
+++ Assets/Superlemonal/Scripts/unstableWall.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unstableWall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other) {
+        if (!other.transform.CompareTag("Player") && other.transform.GetComponent<Rigidbody>() != null)
+        {
+            gameObject.AddComponent<Rigidbody>();
+        }
+    }
+}
